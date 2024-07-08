9e13381461e4222116b370cccd5318b128555739
IdrisI21 <idrisiritas21@gmail.com>
2024-07-08 03:43:38 +0200

    Add plot title

--- r_proj/nutrition_kcal_correlation.R
+++ r_proj/nutrition_kcal_correlation.R
@@ -9,13 +9,14 @@ model <- lm(Energy..kcal. ~ Carbohydrates + Fats + Protein, data = filtered_food
 estimate_weights <- model$coefficients
 
 filtered_food_df$kcal.estimation <- filtered_food_df$Carbohydrates*estimate_weights[2] + filtered_food_df$Fats*estimate_weights[3] + filtered_food_df$Protein*estimate_weights[4] + estimate_weights[1]
-filtered_food_df$estimation.difference <- abs(filtered_food_df$Energy..kcal. - filtered_food_df$kcal.estimation)
+filtered_food_df$estimation.difference <- filtered_food_df$kcal.estimation-filtered_food_df$Energy..kcal.
 
 p <- ggplot(data = filtered_food_df, aes(x=Energy..kcal., y=kcal.estimation))+
   geom_point(alpha=0.25)+
   xlim(0, 1000)+
   xlab("Energy (kcal)")+
   ylab("Estimated calories (kcal)")+
+  ggtitle("True vs Estimated Caloric energy")+
   theme(
     text = element_text(size=16),
     axis.text = element_text(size=16)
