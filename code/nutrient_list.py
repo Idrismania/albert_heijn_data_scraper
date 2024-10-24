cfe54f1cd431e8d75c5909f3d6153ac9394f4e6f
IdrisI21 <idrisiritas21@gmail.com>
2024-10-24 19:26:40 +0200

    Add list of unique nutrient labels in web data

---
+++ code/nutrient_list.py
@@ -0,0 +1,21 @@
+nutrition_labels = ['Energie (kcal)', 'Energie (kJ)', 'Vet', 'waarvan verzadigd', 'waarvan onverzadigd', 'Koolhydraten', 'waarvan suikers',
+                    'Voedingsvezel', 'Eiwitten', 'Zout', 'Vitamine C', 'Kalium/Potassium', 'waarvan enkelvoudig onverzadigd',
+                    'waarvan meervoudig onverzadigd', 'Alcohol', 'Calcium', 'Vitamine B2 / Riboflavine',
+                    'Vitamine B12 / Cyano-Cobalamine', 'Fosfor', 'Ijzer', 'Vitamine A', 'Vitamine D', 'Vitamine B11 / Foliumzuur',
+                    'Jodium', 'Vitamine E', 'Linolzuur', 'Alfa-Linoleenzuur', 'Vitamine B3 / Niacine', 'Magnesium', 'Zink',
+                    'Omega 3 Vetzuren', 'Vitamine B6 / Pyridoxine', 'Vitamine B1 / Thiamine', 'Vitamine K',
+                    'Vitamine B5 / Pantotheenzuur', 'Koper', 'Natrium', 'waarvan polyolen', 'Seleen', 'Vitamine H / Biotine',
+                    'waarvan toegevoegde suikers', 'Omega 6 Vetzuren', 'Folaat', 'Mangaan', 'Chloride', 'Sulfaat', 'Nitraten',
+                    'Bicarbonaat', 'Siliciumdioxide (Sio2)', 'Ash', 'Lactose', 'Glucose', 'Maltodextrine', 'Vitamin D2',
+                    'Vitamine B7 / Inositol', 'Molybdeen', 'Seleen (Organisch)', 'Eicosapentaeenzuur', 'Docosahexaeenzuur',
+                    'waarvan zetmeel', 'Cholesterol', 'Transvet', 'Organic Acids, Total', 'Ph, Hydrogen Ion Concentration',
+                    'Fluoride', 'Carnitine', 'Choline', 'Taurine', 'Nucleotide', 'Arachidonzuur', 'Wei-Eiwit', 'Caseïne',
+                    'Palmitinezuur', 'Galactose In Voedingsvezel', 'Fructose In Voedingsvezel', 'Chroom',
+                    'Tafelzout Gemengd Met Een Kleine Hoeveelheid Van Verschillende Jodiumbevattende Zouten',
+                    'Betaglucanen',
+                    'Vitamin A; Calculated By Summation Of The Vitamin A Activities Of Retinol And The Active Carotenoids',
+                    'Niacine Equivalent', 'Vitamine D3', 'Galactooligosaccharides (Gos)', 'Vita K-1', 'Safranal', 'Omega 9 Vetzuren',
+                    'Collageen hydrolysaten', 'Plantaardige Eiwitten', 'Oligomere proanthocyanidinen (OPC)']
+
+if __name__ == "__main__":
+    pass
